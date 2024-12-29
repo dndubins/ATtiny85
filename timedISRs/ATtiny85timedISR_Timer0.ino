cf24bbde5780e9cb5dfbc52cc3939f0d2be8071f
David Dubins <d.dubins@utoronto.ca>
2024-12-29 03:17:02 -0500

    Add files via upload

--- timedISRs/ATtiny85timedISR_Timer0.ino
+++ timedISRs/ATtiny85timedISR_Timer0.ino
@@ -22,55 +22,55 @@ ATTINY85 digital pin 3 -> Arduino Uno pin 0 RX
 ATTINY85 digital pin 4 -> Arduino Uno pin 1 TX
 */
 
-#include <SoftwareSerial.h> // if needed
+#include <SoftwareSerial.h>  // if needed
 
-volatile bool report=false;       // keep track if ISR is triggered
-volatile int reading=0;           // to hold analog reading
-volatile int cycles=0;            // to hold total# cycles ISR ran
-int reportingTime=2000;           // data collection interval (in milliseconds)
+volatile bool report = false;  // keep track if ISR is triggered
+volatile int reading = 0;      // to hold analog reading
+volatile int cycles = 0;       // to hold total# cycles ISR ran
+int reportingTime = 2000;      // data collection interval (in milliseconds)
 
-#define rxPin 3    // this is physical pin 2 on ATtiny85
-#define txPin 4    // this is physical pin 3 on ATtiny85
+#define rxPin 3  // this is physical pin 2 on ATtiny85
+#define txPin 4  // this is physical pin 3 on ATtiny85
 
 SoftwareSerial mySerial(rxPin, txPin);
 
-void setup(){
+void setup() {
   mySerial.begin(9600);
   setTimer0();
 }
- 
-void loop(){
-  if(report){
-    mySerial.println(reading);   // you shouldn't put serial or timed commands inside ISRs
-    report=false;
+
+void loop() {
+  if (report) {
+    mySerial.println(reading);  // you shouldn't put serial or timed commands inside ISRs
+    report = false;
   }
 }
 
-void setTimer0(){
-  // CTC Match Routine using Timer 0 (ATtiny85)
-  // Formula: frequency=fclk/((OCR0A+1)*N)
-  cli();                      // clear interrupts
-  GTCCR = _BV(PSR0);          // reset the prescaler for Timer0
-  TIMSK |= _BV(OCIE0A);       // enable interrupt on Compare Match A for Timer0
-  TCCR0A |= _BV(WGM01);       // Set CTC mode (WGM01 = 1, WGM00 = 0)
-  //TCCR0B = _BV(WGM02) | _BV(CS00);  // prescaler=1
-  //TCCR0B = _BV(WGM02)  | _BV(CS01);  // prescaler=8
-  TCCR0B = _BV(WGM02) | _BV(CS01) | _BV(CS00);  // prescaler=64
-  //TCCR0B = _BV(WGM02) | _BV(CS02);  // prescaler=256
-  //TCCR0B = _BV(WGM02) | _BV(CS02) | _BV(CS00);  // prescaler=1024
-  OCR0A = 124; // Set betw 1-255 (prescaler=64, OCR1C=124 -->  1 kHz)
-  sei();       // enable interrupts
+void setTimer0() {
+   // CTC Match Routine using Timer 0 (ATtiny85)
+   // Formula: frequency=fclk/((OCR0A+1)*N)
+    cli();  // clear interrupts
+    GTCCR = _BV(PSR0); // reset the prescaler for Timer0
+    TIMSK |= _BV(OCIE0A);  // enable interrupt on Compare Match A for Timer0
+    TCCR0A = 0;  // Normal mode (no PWM)
+    //TCCR0B |= _BV(WGM02) | _BV(CS00);  // prescaler=1
+    //TCCR0B |= _BV(WGM02) | _BV(CS01);  // prescaler=8
+    TCCR0B |= _BV(WGM02) | _BV(CS01) | _BV(CS00);  // prescaler=64
+    //TCCR0B |= _BV(WGM02) | _BV(CS02);  // prescaler=256
+    //TCCR0B |= _BV(WGM02) | _BV(CS02) | _BV(CS00);  // prescaler=1024
+    OCR0A = 124;  // Set betw 1-255 (prescaler=64, OCR0A=124 -->  1kHz)
+    sei();  // enable interrupts
 }
-  
-ISR(TIMER0_COMPA_vect){
+
+ISR(TIMER0_COMPA_vect) {
   //This ISR will run 1000 times per second. We can keep track of the number of times it ran
   //to report a reading.
   //Make ISRs as simple and as short as possible. Any global arrays changed should be declared
   //as volatile.
   cycles++;
-  if(cycles>=reportingTime){  // (2000 msec/1000) x 1000 cycles/sec = 2000 cycles.
-    reading=analogRead(A1); // take an analog reading from pin A1 
-    report=true;            // flag that datum is ready (just for reporting)
-    cycles=0; // reset the number of cycles
+  if (cycles >= reportingTime) {  // (2000 msec/1000) x 1000 cycles/sec = 2000 cycles.
+    reading = analogRead(A1);     // take an analog reading from pin A1
+    report = true;                // flag that datum is ready (just for reporting)
+    cycles = 0;                   // reset the number of cycles
   }
 }
