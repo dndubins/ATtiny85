9edde1ab126fa5aeb044d862d96e895d454fbb96
David Dubins <d.dubins@utoronto.ca>
2026-02-21 10:43:35 -0500

    Update fast pin modes for ATtiny84

--- LabToy85/LabToy85.ino
+++ LabToy85/LabToy85.ino
@@ -57,18 +57,35 @@
 #define OSC_N 94   // Calibrated value for OSCCAL (comment out if not calibrating)
 #define TCAL 23.4  // Calibrated value for core temperature (0.0 if not calibrating)
 
-// For fast pin modes, writes, and reads for the ATtiny85 (pins 0-5):
-// Modes: 0=INPUT, 1=OUTPUT, 2=INPUT_PULLUP
+// Fast pin modes, writes, and reads for the ATtiny84 (digital pins 0-11)
+// Modes: 0 = INPUT, 1 = OUTPUT, 2 = INPUT_PULLUP
 #define pinModeFast(p, m) \
-  if ((m)&1) DDRB |= 1 << (p); \
-  else DDRB &= ~(1 << (p)); \
-  if (!((m)&1)) ((m)&2 ? PORTB |= 1 << (p) : PORTB &= ~(1 << (p)))
+  do { \
+    if ((p) <= 7) { \
+      if ((m)&1) DDRA |= (1 << (p)); \
+      else DDRA &= ~(1 << (p)); \
+      if (!((m)&1)) ((m)& 2 ? PORTA |= (1 << (p)) : PORTA &= ~(1 << (p))); \
+    } else if ((p) >=8 && (p) <= 11) { \
+      if ((m)&1) DDRB |= (1 << ((p) - 8)); \
+      else DDRB &= ~(1 << ((p) - 8)); \
+      if (!((m)&1)) ((m)& 2 ? PORTB |= (1 << ((p) - 8)) : PORTB &= ~(1 << ((p) - 8))); \
+    } \
+  } while(0)
 
 #define digitalWriteFast(p, v) \
-  (v) ? PORTB |= 1 << (p) : PORTB &= ~(1 << (p))
+  do { \
+    if ((p) <= 7) { \
+      (v) ? PORTA |= (1 << (p)) : PORTA &= ~(1 << (p)); \
+    } else if ((p) >= 8 && (p) <= 11) { \
+      (v) ? PORTB |= (1 << ((p) - 8)) : PORTB &= ~(1 << ((p) - 8)); \
+    } \
+  } while(0)
 
 #define digitalReadFast(p) \
-  (PINB & (1 << (p)))
+  (((p) <= 7) ? \
+    ((PINA & (1 << (p))) ? 1 : 0) : \
+   ((p) >= 8 && (p) <= 11) ? \
+    ((PINB & (1 << ((p) - 8))) ? 1 : 0) : 0)
 
 #include <avr/sleep.h>  // sleep library
 #include <avr/power.h>  // power library
